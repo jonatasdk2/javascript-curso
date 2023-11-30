805f6f8567c23d6b032f92199e75733b20249cb4
jonatasdk2 <jonataspfbarbosa@gmail.com>
2023-11-30 20:16:53 -0300

    condições simples e compostas

---
+++ aula0011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){ //condição simples
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`dirija sempre usando cinto de sehurança!`)
