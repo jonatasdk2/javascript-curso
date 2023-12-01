11a80b169a5129d660b5c5a7a543702284e8d535
jonatasdk2 <jonataspfbarbosa@gmail.com>
2023-12-01 19:43:42 -0300

    criando site com exemplos de condicionais

---
+++ aula012ex/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar(params) {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min =  data.getMinutes()
+
+    msg.innerHTML = `agora são ${hora} horas ${min} minutos.`
+    
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manhã.png'
+        document.body.style.background ='#d46d1b'
+    } else if (hora >= 12 && hora <=18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background ='#80c6fb'
+    }else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background ='#181f28'
+    }
+    
+}
+
