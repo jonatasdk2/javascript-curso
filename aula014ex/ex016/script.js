c90c67d738f7be4e682e254d223973b4e62d4fd3
jonatasdk2 <jonataspfbarbosa@gmail.com>
2023-12-04 23:14:50 -0300

    atividade contador com estrutura de repetição for

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,65 +1,41 @@
-function verificar(params) {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('ano')
-    var res = document.getElementById('res')
-
-    if (fano.value.length == 0 || fano.value > ano) {
-        window.alert("[erro] verifique os dados e tente novamente!")
-    } else {
-        var fsex = document.getElementsByName('sex')
-        var idade = ano - fano.value
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id','foto')
-        img.style.textAlign = 'center'
-        img.style.paddingTop = '20px'
-        
-         
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            
-            if (idade>= 0 && idade <10) {
-                img.setAttribute('src','imagens/bebe homem.png')
-                
-            } else if(idade >= 10 && idade < 20) {
-
-                img.setAttribute('src','imagens/homem jovem.png')
-                
-            }
-            else if (idade >=20 && idade <50) {
-
-                img.setAttribute('src','imagens/homem adulto.png')
-                
-            }
-            else if(idade >= 50 && idade <150) {
-
-                img.setAttribute('src','imagens/homem idoso.png')
-            }
-        } else if(fsex[1].checked) {
-            genero = 'mulher'
-            
-            if (idade>= 0 && idade <10) {
-                img.setAttribute('src','imagens/bebe mulher.png')
-                
-            } else if(idade >= 10 && idade < 20) {
-
-                img.setAttribute('src','imagens/mulher jovem.png')
-                
-            }
-            else if (idade >=20 && idade <50) {
-
-                img.setAttribute('src','imagens/mulher adulta.png')
-                
-            }
-            else if(idade >= 50 && idade <150) {
-
-                img.setAttribute('src','imagens/mulher idosa.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `detectamos ${genero} com ${idade} anos`
-        res.appendChild(img)
-    }
+function calcular(params) {
     
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var result = document.getElementById('res')
+    
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+      alert('erro')
+        
+    }else{
+      result.innerHTML = 'contando: <br/>'
+      let i =Number(ini.value)
+      let f =Number(fim.value)
+      let p =Number(passo.value)
+
+      if (p <= 0) {
+        alert('passo invalido considerando passo como 1')
+        p = 1
+        
+      }
+
+      if (i < f) { //contagem crescente
+        for (let c = i; c <= f; c += p) {
+          result.innerHTML += `${c} \u{1f449}`
+          
+        }
+        
+      }else{ // contagem regressiva
+        for (let c = i; c >=f; c -= p) {
+          result.innerHTML += `${c} \u{1f449}`
+          
+        }
+
+      }
+     
+      result.innerHTML += `\u{1f3c1}`
+    }
 }
+
