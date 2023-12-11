eb0c8be0f29f92094ae6318d4e7bf7b6515e5a0c
jonatasdk2 <jonataspfbarbosa@gmail.com>
2023-12-10 23:44:58 -0300

    atividades com função

--- aula016/função004.js
+++ aula016/função004.js
@@ -10,3 +10,20 @@ function fatorial(n) {
 
 
 console.log(fatorial(5)) 
+
+
+
+let valores =[1,2,3,4,5]
+let maior = valores[0]
+let menor = valores[0]
+
+for (let c = 0; c < valores.length; c++) {
+  if (valores[c] > maior) {
+    maior= valores[c]
+  }  
+  if (valores[c] < menor) {
+    menor= valores[c]
+  }
+    
+}
+console.log(`maior é ${maior} e menor é ${menor}`)
--- aula016ex/script.js
+++ aula016ex/script.js
@@ -28,14 +28,52 @@ function inlist(n,l) {
 
 function adicionar() {
   if (isnumero(num.value) && !inlist(num.value,valores)) {
-    alert('tudo ok')
+   valores.push(Number(num.value))
+   let item = document.createElement('option')
+   item.text=`valor ${num.value} adicionado`
+   list.appendChild(item)
+   res.innerHTML = ''
     
   } else {
     alert('valor invalido ou já encontrado na lista')
   }
     
-
+  num.value=''
+  num.focus()
 
   
 }
+
+function finalizar(){ 
+  if (valores.length == 0) {
+    alert('adicione um valor a lista')
+    
+  } else {
+    let tot = valores.length;
+    let maior = valores[0]
+    let menor = valores [0]
+    let soma = 0;
+    let media = 0
+    for (let c = 0; c < valores.length; c++) {
+     soma +=valores[c] 
+    if (valores[c] > maior) {
+    maior= valores[c]
+    }  
+    if (valores[c] < menor) {
+    menor= valores[c]
+    }
+    media= soma/tot
+    }
+
+    res.innerHTML= ''
+    res.innerHTML += `<p>o total de números cadastrados é ${tot}</p>`
+    res.innerHTML += `<p>o maior numero é ${maior}</p>`
+    res.innerHTML += `<p>o menor numero é ${menor}</p>`
+    res.innerHTML += `<p>a soma de todos os números é ${soma}</p>`
+    res.innerHTML += `<p>a média de todos os números é ${media}</p>`
+  }  
+    
+}  
+  
+  
 
